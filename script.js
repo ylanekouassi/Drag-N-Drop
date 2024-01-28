ae6a916c57c587f8c00bb74736a3d38b53a727cc
ylanekouassi <121358143+ylanekouassi@users.noreply.github.com>
2024-01-28 12:56:23 -0500

    drag to the left is working

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-let lists = document.getElementsByClassName("list");
+let lists = document.querySelectorAll(".item");
 let leftTable = document.getElementById("left");
-let rightTable = document.getElementById("right")
+let rightTable = document.getElementById("right");
+
+for(item of lists){
+    item.addEventListener("dragstart", function(e){
+        let selected = e.target;
+
+        rightTable.addEventListener("dragover", function(e){
+            e.preventDefault();
+        });
+        rightTable.addEventListener("drop", function(e){
+            if(selected !== null)
+            rightTable.append(selected);
+            selected = null;
+        })
+    })
+
+
+
+    } 
+        
